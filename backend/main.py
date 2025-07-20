9ce9da3bf36770ad38b5dad44b6a6734c33d9d2e
Khang Phan <93765633+khangpt2k6@users.noreply.github.com>
2025-07-20 14:40:17 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -608,4 +608,4 @@ if __name__ == "__main__":
     import os
 
     port = int(os.environ.get("PORT", 8000))  # default to 8000 if PORT is not set
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
